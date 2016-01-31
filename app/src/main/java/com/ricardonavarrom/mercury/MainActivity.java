0d4fd6c1a623422906e0ac2e0b474a948ec1357d
Ricardo Navarro Moral <ricardonavarromoral@gmail.com>
2016-01-31 20:13:40 +0100

    [NEW] De momento en la landing page ocultaremos el fabutton.

--- app/src/main/java/com/ricardonavarrom/mercury/MainActivity.java
+++ app/src/main/java/com/ricardonavarrom/mercury/MainActivity.java
@@ -8,7 +8,6 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -30,13 +29,7 @@ public class MainActivity extends AppCompatActivity implements ArtistsFragment.C
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.main_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setVisibility(View.GONE);
 
         artistsRankingNumber = getPreferredArtistsRankingNumber();
         artistsRankingGenre = getPreferredArtistsRankingGenre();
