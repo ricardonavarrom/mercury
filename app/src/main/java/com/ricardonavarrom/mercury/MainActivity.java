46076f5072cf3a2d69a215f10c26feccc83b8e7a
Ricardo Navarro Moral <ricardonavarromoral@gmail.com>
2016-02-06 20:36:00 +0100

    [UPD] Personalizando la UI.

--- app/src/main/java/com/ricardonavarrom/mercury/MainActivity.java
+++ app/src/main/java/com/ricardonavarrom/mercury/MainActivity.java
@@ -8,6 +8,7 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -36,7 +37,16 @@ public class MainActivity extends AppCompatActivity implements ArtistsFragment.C
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.main_fab);
-        fab.setVisibility(View.GONE);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    runFloatingActionButtonAction(view);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         artistsRankingNumber = getPreferredArtistsRankingNumber();
         artistsRankingGenre = getPreferredArtistsRankingGenre();
@@ -52,17 +62,13 @@ public class MainActivity extends AppCompatActivity implements ArtistsFragment.C
     @Override
     protected void onResume() {
         super.onResume();
-        int actualArtistsRankingNumber = getPreferredArtistsRankingNumber();
-        String actualArtistsRankingGenre = getPreferredArtistsRankingGenre();
         try {
-            if (actualArtistsRankingNumber != artistsRankingNumber
-                    || !actualArtistsRankingGenre.equals(artistsRankingGenre)
-                    || artistsRankIsExpired()) {
+            if (isNecessaryRefreshRanking()) {
                 ArtistsFragment artistsFragment = (ArtistsFragment)
                         getSupportFragmentManager().findFragmentById(R.id.content_fragment_artists);
                 if (artistsFragment != null) {
-                    artistsFragment.onRefreshNecessary(actualArtistsRankingNumber,
-                            actualArtistsRankingGenre, isOnline());
+                    artistsFragment.onRefreshNecessary(getPreferredArtistsRankingNumber(),
+                            getPreferredArtistsRankingGenre(), isOnline());
                 }
             }
         } catch (ParseException e) {
@@ -150,5 +156,33 @@ public class MainActivity extends AppCompatActivity implements ArtistsFragment.C
         calendar.set(Calendar.SECOND, 0);
 
         return calendar.getTime();
+    }
+
+    private boolean isNecessaryRefreshRanking() throws ParseException {
+        int actualArtistsRankingNumber = getPreferredArtistsRankingNumber();
+        String actualArtistsRankingGenre = getPreferredArtistsRankingGenre();
+
+        return actualArtistsRankingNumber != artistsRankingNumber
+                || !actualArtistsRankingGenre.equals(artistsRankingGenre) || artistsRankIsExpired();
+    }
+
+    private void runFloatingActionButtonAction(View view) throws ParseException {
+        String actualGenre = getPreferredArtistsRankingGenre();
+        String snackBarText = actualGenre.equals(getString(R.string.pref_artists_rank_genre_default))
+                ? getString(R.string.echonest_data_message_all_genres)
+                : getString(R.string.echonest_data_message) + ": " + getPreferredArtistsRankingGenre();
+        snackBarText = snackBarText.concat(". " + getString(R.string.last_data_update_date)
+                + ": " + getLastRankUpdateDateString());
+
+        Snackbar.make(view, snackBarText, Snackbar.LENGTH_LONG).show();
+    }
+
+    private String getLastRankUpdateDateString() throws ParseException {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(getExpirationDateRank());
+        calendar.add(Calendar.DAY_OF_YEAR, -1);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd", Locale.ENGLISH);
+
+        return dateFormat.format(calendar.getTime());
     }
 }
