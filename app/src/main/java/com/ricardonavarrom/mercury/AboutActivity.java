6ee75d4791b312747164c6453edd0c7e48086336
Ricardo Navarro Moral <ricardonavarromoral@gmail.com>
2016-02-12 15:56:00 +0100

    [FIX] El botón home de la barra de navegación en la activity de acerca de... siempre llevará atrás y no arriba.

--- app/src/main/java/com/ricardonavarrom/mercury/AboutActivity.java
+++ app/src/main/java/com/ricardonavarrom/mercury/AboutActivity.java
@@ -3,6 +3,7 @@ package com.ricardonavarrom.mercury;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -15,4 +16,16 @@ public class AboutActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
